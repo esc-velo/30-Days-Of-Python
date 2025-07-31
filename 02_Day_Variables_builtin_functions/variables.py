9481c98830f4d975a2ce309f47210d73352ee663
Rahini Raneru <rahiniraneru@yahoo.com>
2025-07-31 15:26:46 +0530

    day2

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,40 +1,58 @@
 
-# Variables in Python
+# Day 2: 30 Days of python programming
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname':'Asabeneh', 
-    'lastname':'Yetayeh', 
-    'country':'Finland',
-    'city':'Helsinki'
-    }
+first_name = "Rahini"
+last_name = "Raneru"
+full_name = "Rahini Raneru"
+country = "India"
+city = "Hyderabad"
+age = 18
+year = 2025
+is_married = False
+is_true = True
+is_light_on = True
 
-# Printing the values stored in the variables
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
+DoB, college, yearinCollege, is_smart = '02-06-2007', 'Manipal University', 'second', True
 
-# Declaring multiple variables in one line
 
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+print(type(first_name))
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(DoB)
 
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+
+print(len(first_name))
+
+
+if (len(first_name) > len(last_name)) :
+    print("First name wins")
+else :
+    print("Last name wins")
+
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+rem = num_one % num_two
+exp = num_one ** num_two
+
+rad = 30.0
+area_of_circle = 3.14 * rad * rad
+circum = 3.14 * 2 * rad
+print("Area : ", area_of_circle)
+print("Circumference : ", circum)
+
+r = input("Enter radius: ")
+area = 3.14 * float(r) * float(r)
+print(f"Area : {area:.2f}")
+
+
